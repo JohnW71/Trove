cc88a573893a4c9b1bf09d41f3155beafbd8a605
John Wingfield <john@johnwingfield.com>
2018-06-13 22:14:01 +0200

    Add header file & AES library

---
+++ trove.h
@@ -0,0 +1,36 @@
+#ifndef TROVE_H
+#define TROVE_H
+#define MAXTITLE 21
+#define MAXMISC 51
+#define MAXLINE 80
+#define MAXID 21
+#define MAXPW 21
+#define MINPW 6
+#define MINCHARS 0
+#define MAXCHARS 6
+
+#include <stdio.h>
+#include <stdlib.h>	// exit(), rand()
+#include <string.h>	// strcmp(), strcpy()
+#include <time.h>	// time()
+#include <ctype.h>	// isupper(), ispunct(), isdigit()
+
+void list();
+void add();
+void find();
+void edit();
+void delete();
+void clipboard();
+void readEntries();
+void saveEntries();
+void generatePassword(char *);
+void setDBpassword();
+void removeDBpassword();
+void readSettings();
+void writeSettings();
+void updateSettings();
+void setPasswordSize();
+void setMinSpecial();
+void setMinNumeric();
+void setMinUppercase();
+#endif
