ef2a33ed3b8855c5beed38a25f95681b1294d888
John Wingfield <john@johnwingfield.com>
2018-10-02 09:16:19 +0200

    Move definitions to header

--- gui.c
+++ gui.c
@@ -1,66 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+#include "gui.h"
+
+//TODO test if message loop should be While or IF
 //TODO fix sorting, listbox does not match saved order
-//TODO allow tabbing on Add window
-
-#include <stdio.h>
-#include <stdbool.h>
-#include <windows.h>
-
-#define ID_MAIN_ADD 1
-#define ID_MAIN_EDIT 2
-#define ID_MAIN_DELETE 3
-#define ID_MAIN_SETTINGS 4
-#define ID_MAIN_QUIT 5
-#define ID_MAIN_TEXTBOX 6
-#define ID_MAIN_FIND 7
-#define ID_MAIN_LISTBOX 8
-
-#define ID_EDIT_TITLE_LABEL 10
-#define ID_EDIT_TITLE 11
-#define ID_EDIT_ID_LABEL 12
-#define ID_EDIT_ID 13
-#define ID_EDIT_PW_LABEL 14
-#define ID_EDIT_PW 15
-#define ID_EDIT_MISC_LABEL 16
-#define ID_EDIT_MISC 17
-#define ID_EDIT_CANCEL 18
-#define ID_EDIT_OK 19
-
-#define MAXTITLE 21
-#define MAXID 21
-#define MAXPW 21
-#define MAXMISC 51
-#define MAXLINE 110
-
-// #define MINPW 6
-// #define MINCHARS 0
-// #define MAXCHARS 6
-// #define MAXNAME 10
-// #define KEYSIZE 32
-
-void loadEntries();
-void saveEntries();
-void centerWindow(HWND);
-void updateListbox();
-void addEntry();
-void editEntry();
-void deleteEntry();
-void findEntry();
-void settings();
-
-LRESULT CALLBACK mainWndProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK addWndProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK editWndProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK settingsWndProc(HWND, UINT, WPARAM, LPARAM);
-
-struct Entry
-{
-	wchar_t title[MAXTITLE];
-	wchar_t id[MAXID];
-	wchar_t pw[MAXPW];
-	wchar_t misc[MAXMISC];
-} *entries;
+//TODO Add/Edit should have a "Generate password" icon
+//TODO implement Find
+//TODO design Settings
+//TODO implement Settings
+//TODO implement encryption
 
 static char tempFile[] = "temp.db";
 static bool running = true;
@@ -534,7 +482,6 @@ LRESULT CALLBACK editWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
-//TODO if this is all there is, move back to mainWndProc
 void deleteEntry()
 {
 	if (selectedRow != LB_ERR)
