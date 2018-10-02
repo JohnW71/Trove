4d372b490a4dfbe04b0a9b6379c194162b960f5e
Author Name <johnwingfield@protonmail.com>
2018-10-02 09:16:19 +0200

    Move definitions to header

---
+++ gui.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <windows.h>
+
+#define ID_MAIN_ADD 1
+#define ID_MAIN_EDIT 2
+#define ID_MAIN_DELETE 3
+#define ID_MAIN_SETTINGS 4
+#define ID_MAIN_QUIT 5
+#define ID_MAIN_TEXTBOX 6
+#define ID_MAIN_FIND 7
+#define ID_MAIN_LISTBOX 8
+
+#define ID_EDIT_TITLE_LABEL 10
+#define ID_EDIT_TITLE 11
+#define ID_EDIT_ID_LABEL 12
+#define ID_EDIT_ID 13
+#define ID_EDIT_PW_LABEL 14
+#define ID_EDIT_PW 15
+#define ID_EDIT_MISC_LABEL 16
+#define ID_EDIT_MISC 17
+#define ID_EDIT_CANCEL 18
+#define ID_EDIT_OK 19
+
+#define MAXTITLE 21
+#define MAXID 21
+#define MAXPW 21
+#define MAXMISC 51
+#define MAXLINE 110
+
+// #define MINPW 6
+// #define MINCHARS 0
+// #define MAXCHARS 6
+// #define MAXNAME 10
+// #define KEYSIZE 32
+
+void loadEntries();
+void saveEntries();
+void centerWindow(HWND);
+void updateListbox();
+void addEntry();
+void editEntry();
+void deleteEntry();
+void findEntry();
+void settings();
+
+LRESULT CALLBACK mainWndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK addWndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK editWndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK settingsWndProc(HWND, UINT, WPARAM, LPARAM);
+
+struct Entry
+{
+	wchar_t title[MAXTITLE];
+	wchar_t id[MAXID];
+	wchar_t pw[MAXPW];
+	wchar_t misc[MAXMISC];
+} *entries;
